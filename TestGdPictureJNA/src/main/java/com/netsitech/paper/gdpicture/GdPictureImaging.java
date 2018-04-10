ae7aff8883c8873932653a69e924d7bef1822e1a
Matthias Bläsing <mblaesing@doppel-helix.eu>
2018-04-10 22:13:21 +0200

    Workaround for JNA limitation/bug regarding SAFEARRAY ByRef

--- TestGdPictureJNA/src/main/java/com/netsitech/paper/gdpicture/GdPictureImaging.java
+++ TestGdPictureJNA/src/main/java/com/netsitech/paper/gdpicture/GdPictureImaging.java
@@ -4,13 +4,16 @@ import java.io.File;
 
 import com.netsitech.paper.gdpicture.enums.DocumentFormat;
 import com.netsitech.paper.gdpicture.enums.GdPictureStatus;
+import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.OaIdl.SAFEARRAY;
-import com.sun.jna.platform.win32.OaIdlUtil;
 import com.sun.jna.platform.win32.Variant;
 import com.sun.jna.platform.win32.Variant.VARIANT;
 import com.sun.jna.platform.win32.WTypes;
 import com.sun.jna.platform.win32.COM.COMLateBindingObject;
+import com.sun.jna.platform.win32.WTypes.VARTYPE;
+import com.sun.jna.platform.win32.WinDef.PVOID;
 import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.PointerByReference;
 
 public class GdPictureImaging extends COMLateBindingObject {
 
@@ -95,21 +98,62 @@ public class GdPictureImaging extends COMLateBindingObject {
 	// return getAsTIFF(TiffCompression.TiffCompressionCCITT4);
 	// }
 
-	public byte[] getAs(DocumentFormat imageFormat, int encoderParameter) throws GdPictureException {
+            public byte[] getAs(DocumentFormat imageFormat, int encoderParameter) throws GdPictureException {
+            // Initialize SAFEARRAY as input
+            SAFEARRAY safeArray = SAFEARRAY.createSafeArray(new WTypes.VARTYPE(Variant.VT_UI1), 1);
+            try {
+                // This is a hack to work around a limit in the definition
+                // of VARIANT in JNA - a SAFEARRAY ByRef is not correctly
+                // handled.
+                //
+                // Initialize a VARIANT
+                VARIANT dataInOut = new VARIANT();
+                // Get a Pointer to the SAFEARRAY structure (**SAFEARRAY)
+                PointerByReference pbr = new PointerByReference(safeArray.getPointer());
+                // First ensure the value reaches the VARIANT structure by
+                // using the generic VT_BYREF marker
+                dataInOut.setValue(new VARTYPE(Variant.VT_BYREF), new PVOID(pbr.getPointer()));
+                // Now override the type to make the library accept it
+                dataInOut.setVarType((short) (Variant.VT_BYREF | Variant.VT_ARRAY | Variant.VT_UI1));
 
-		SAFEARRAY safeArray = SAFEARRAY.createSafeArray(new WTypes.VARTYPE(Variant.VT_UI1), 1);
-		VARIANT dataInOut = new VARIANT();
-		dataInOut.setValue(/* Variant.VT_BYREF | */ Variant.VT_ARRAY, safeArray);
+                IntByReference length = new IntByReference();
+                VARIANT lengthInOut = new VARIANT();
+                lengthInOut.setValue(Variant.VT_BYREF | Variant.VT_INT, length);
 
-		IntByReference length = new IntByReference();
-		VARIANT lengthInOut = new VARIANT();
-		lengthInOut.setValue(Variant.VT_BYREF | Variant.VT_INT, length);
+                System.out.println(pbr.getPointer() + " - " + pbr.getValue());
 
-		VARIANT status = this.invoke("SaveAsByteArray", new VARIANT[] { imageId, dataInOut, lengthInOut, new VARIANT(imageFormat.getValue()), new VARIANT(encoderParameter) });
-		checkStatus(status);
+                VARIANT status = this.invoke("SaveAsByteArray", new VARIANT[]{imageId, dataInOut, lengthInOut, new VARIANT(imageFormat.getValue()), new VARIANT(encoderParameter)});
+                checkStatus(status);
 
-		return (byte[]) OaIdlUtil.toPrimitiveArray(safeArray, false);
-	}
+                if (!pbr.getValue().equals(safeArray.getPointer())) {
+                    // The library returned a new array - we need to destroy
+                    // the old one and wrap the new one
+                    safeArray.destroy();
+                    safeArray = new SAFEARRAY(pbr.getValue());
+                }
+                System.out.println(pbr.getPointer() + " - " + pbr.getValue());
+
+                // After the call the safeArray structure needs to be
+                // manually updated as JNA can't determine that the structure
+                // needs to be updated
+                safeArray.read();
+
+                // Access the raw data of the safearray. As it is one 
+                // dimensional the backing memory can read linearly
+                Pointer pointer = safeArray.accessData();
+                try {
+                    return pointer.getByteArray(0, safeArray.getUBound(0) - safeArray.getLBound(0) + 1);
+                } finally {
+                    // accessData locks the array and would prevent destructing it.
+                    safeArray.unaccessData();
+                }
+            } finally {
+                // destroy the SAFEARRAY after use
+                if (safeArray != null) {
+                    safeArray.destroy();
+                }
+            }
+        }
 
 	public void saveAsBMP(File destination) throws GdPictureException {
 		VARIANT status = this.invoke("SaveAsBMP", new VARIANT[] { imageId, new VARIANT(destination.getAbsolutePath()) });
